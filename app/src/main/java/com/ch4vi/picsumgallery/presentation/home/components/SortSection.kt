534938b2ef1f9a472bf8aa2e1dd23962d2b9aa06
Xavi Anyó <javiguada60@hotmail.com>
2023-08-22 00:59:10 +0200

    Picture gallery UI (#2)
    * update detekt file for compose
    
    * Added compose ui

---
+++ app/src/main/java/com/ch4vi/picsumgallery/presentation/home/components/SortSection.kt
@@ -0,0 +1,70 @@
+package com.ch4vi.picsumgallery.presentation.home.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ch4vi.picsumgallery.R
+import com.ch4vi.picsumgallery.domain.model.ImageOrder
+import com.ch4vi.picsumgallery.domain.model.OrderType
+import com.ch4vi.picsumgallery.ui.theme.PicsumGalleryTheme
+
+@Composable
+fun SortSection(
+    modifier: Modifier = Modifier,
+    imageOrder: ImageOrder = ImageOrder.None(OrderType.Ascending),
+    onOrderChange: (ImageOrder) -> Unit
+) {
+    Column(modifier = modifier) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            SortRadioButton(
+                label = stringResource(id = R.string.order_none),
+                isSelected = imageOrder is ImageOrder.None,
+                onSelected = { onOrderChange(ImageOrder.None(imageOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            SortRadioButton(
+                label = stringResource(id = R.string.order_width),
+                isSelected = imageOrder is ImageOrder.Width,
+                onSelected = { onOrderChange(ImageOrder.Width(imageOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            SortRadioButton(
+                label = stringResource(id = R.string.order_height),
+                isSelected = imageOrder is ImageOrder.Height,
+                onSelected = { onOrderChange(ImageOrder.Height(imageOrder.orderType)) }
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            SortRadioButton(
+                label = stringResource(id = R.string.order_ascending),
+                isSelected = imageOrder.orderType is OrderType.Ascending,
+                onSelected = { onOrderChange(imageOrder.copy(OrderType.Ascending)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            SortRadioButton(
+                label = stringResource(id = R.string.order_descending),
+                isSelected = imageOrder.orderType is OrderType.Descending,
+                onSelected = { onOrderChange(imageOrder.copy(OrderType.Descending)) }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SortSectionPreview() {
+    PicsumGalleryTheme {
+        SortSection {}
+    }
+}
