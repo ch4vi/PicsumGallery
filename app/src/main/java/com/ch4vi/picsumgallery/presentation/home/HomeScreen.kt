37db4053754fdd54b9e1e01a007e9155f5f244d3
Xavi Anyo <xavi.anyo@xing.com>
2023-08-21 19:13:10 +0200

    Added home skeleton

--- app/src/main/java/com/ch4vi/picsumgallery/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/ch4vi/picsumgallery/presentation/home/HomeScreen.kt
@@ -1,17 +1,55 @@
 package com.ch4vi.picsumgallery.presentation.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.icons.filled.Sort
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.ch4vi.picsumgallery.R
 import com.ch4vi.picsumgallery.ui.theme.PicsumGalleryTheme
 
 @Composable
 fun HomeScreen(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Scaffold(
+        snackbarHost = { },
+        topBar = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                IconButton(
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.FilterList,
+                        contentDescription = "Filter"
+                    )
+                }
+            }
+        }
+    ) { padding ->
+        // lazy column
+    }
 }
 
 @Preview(showBackground = true)
