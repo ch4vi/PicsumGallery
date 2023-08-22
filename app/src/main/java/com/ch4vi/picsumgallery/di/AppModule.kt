3ef705b0245b09ddad99bda4dddec2491972b2e6
Xavi Anyo <xavi.anyo@xing.com>
2023-08-22 22:10:30 +0200

    Updated di

--- app/src/main/java/com/ch4vi/picsumgallery/di/AppModule.kt
+++ app/src/main/java/com/ch4vi/picsumgallery/di/AppModule.kt
@@ -1,13 +1,18 @@
 package com.ch4vi.picsumgallery.di
 
 import android.app.Application
+import android.content.Context.MODE_PRIVATE
 import androidx.room.Room
+import com.ch4vi.picsumgallery.Constants
 import com.ch4vi.picsumgallery.data.api.NetworkConnectivity
 import com.ch4vi.picsumgallery.data.api.RetrofitConfiguration
 import com.ch4vi.picsumgallery.data.api.datasource.PictureApiDataSource
 import com.ch4vi.picsumgallery.data.api.service.PictureService
 import com.ch4vi.picsumgallery.data.database.AppDatabase
+import com.ch4vi.picsumgallery.data.preferences.datasource.UserPreferencesDatasource
+import com.ch4vi.picsumgallery.data.repository.ConnectivityRepositoryImp
 import com.ch4vi.picsumgallery.data.repository.PicturesRepositoryImp
+import com.ch4vi.picsumgallery.domain.repository.ConnectivityRepository
 import com.ch4vi.picsumgallery.domain.repository.PicturesRepository
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -41,19 +46,31 @@ class AppModule {
     fun provideNetworkConnectivity(app: Application) = NetworkConnectivity(app)
 
     @Provides
+    fun provideUserPreferencesDatasource(app: Application) =
+        UserPreferencesDatasource(
+            app.getSharedPreferences(Constants.PREFERENCES_NAME, MODE_PRIVATE)
+        )
+
+    @Provides
     fun providePictureApiDataSource(service: PictureService): PictureApiDataSource {
         return PictureApiDataSource(service)
     }
 
     @Provides
+    fun provideConnectivityRepository(networkConnectivity: NetworkConnectivity): ConnectivityRepository =
+        ConnectivityRepositoryImp(networkConnectivity)
+
+    @Provides
     fun providePicturesRepository(
         appDatabase: AppDatabase,
         apiDataSource: PictureApiDataSource,
+        preferencesDatasource: UserPreferencesDatasource,
         networkConnectivity: NetworkConnectivity
     ): PicturesRepository =
         PicturesRepositoryImp(
             apiDatasource = apiDataSource,
             databaseDataSource = appDatabase.pictureDao(),
-            networkConnectivity = networkConnectivity
+            preferencesDatasource = preferencesDatasource,
+            networkConnectivity = networkConnectivity,
         )
 }
